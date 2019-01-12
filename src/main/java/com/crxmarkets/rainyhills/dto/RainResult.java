b9808220c2180860e6f65508a334e4c78f89f366
herman.urikh <herman.urikh@aengel.ru>
2019-01-12 20:40:46 +0300

    RainyHills: Fix FindBugs.

--- src/main/java/com/crxmarkets/rainyhills/dto/RainResult.java
+++ src/main/java/com/crxmarkets/rainyhills/dto/RainResult.java
@@ -1,10 +1,39 @@
 package com.crxmarkets.rainyhills.dto;
 
-import lombok.Data;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
-@Data
+import java.util.Arrays;
+
 public class RainResult {
+
     private final int[] sourceArray;
     private final long[] waterToFill;
     private final long volume;
+
+    public RainResult(int[] sourceArray, long[] waterToFill, long volume) {
+        //copy results not to store references to something that can be changed outside, this class should be immutable
+        this.sourceArray = Arrays.copyOf(sourceArray, sourceArray.length);
+        this.waterToFill = Arrays.copyOf(waterToFill, waterToFill.length);
+        this.volume = volume;
+    }
+
+    @SuppressFBWarnings(
+            value = "MALICIOUS_CODE",
+            justification = "this getter is for jackson + tests only, no immutability needed"
+    )
+    public int[] getSourceArray() {
+        return sourceArray;
+    }
+
+    @SuppressFBWarnings(
+            value = "MALICIOUS_CODE",
+            justification = "this getter is for jackson + tests only, no immutability needed"
+    )
+    public long[] getWaterToFill() {
+        return waterToFill;
+    }
+
+    public long getVolume() {
+        return volume;
+    }
 }
